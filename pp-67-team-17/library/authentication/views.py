0d2d741358626b9bd051973d5609c286bf975196
Roman Kurdupel <87699645+rkurdupel@users.noreply.github.com>
2023-10-05 11:35:38 +0300

    Update views.py

--- pp-67-team-17/library/authentication/views.py
+++ pp-67-team-17/library/authentication/views.py
@@ -1,11 +1,19 @@
 # Create your views here.
 from django.shortcuts import render
+from .models import CustomUserManager
 
 def home_page(request):
     return render(request, 'home_page.html')
 def register_page(request):
     if request.method == "POST":
-        return render(request, "home_page.html")
+        first_name = request.POST.get("first_name", "")
+        middle_name = request.POST.get("middle_name", "")
+        last_name = request.POST.get("last_name", "")
+        email = request.POST.get("email", "")
+        password = request.POST.get("password", "")
+
+        CustomUserManager.create_user(email, password, first_name, middle_name, last_name)
+
     return render(request, "register_page.html")
 def login_page(request):
     return render(request, 'login_page.html')
